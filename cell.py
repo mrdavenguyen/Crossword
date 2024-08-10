9be3bf9ebd0231dbd2aa274c64c7254222cadcaa
Criselda Aguilar <criselda.aguilar@gmail.com>
2024-08-10 22:49:27 +0800

    Add cell, grid and word files Move Cell, Grid and Word classes from Crossword (main) to each separate files for code organization and best practice Refactor get_word_list from function to a GetWordList class Remove unused import

---
+++ cell.py
@@ -0,0 +1,17 @@
+class Cell:
+    def __init__(self):
+
+        self.letter = None # None for blank, "#" for black square, or a letter
+        self.numbering = None # None or the number of the clue
+        self.num_across = None # Reference to the across word number
+        self.num_down = None # Reference to the down word number
+
+    def __str__(self):
+        """
+        Displays instance variables when the cell object is printed.
+        """
+        letter = f"self.letter = {self.letter}"
+        number = f"self.numbering = {self.numbering}"
+        across = f"self.num_across = {self.num_across}"
+        down = f"self.num_down = {self.num_down}"
+        return f"{letter}, {number}, {across}, {down}"
--- get_word_lists.py
+++ get_word_lists.py
@@ -1,18 +1,24 @@
 import random
 
-def main():
-    wordlists = create_wordlists()
+# def main():
+#     word_lists = create_word_lists()
 
-def create_wordlists():
-    wordlists = {i: [] for i in range(3, 16)}
-    with open("words_alpha.txt") as file:
-        for word in file:
-            word = word.strip()
-            if 3 <= len(word) <= 15:
-                wordlists[len(word)].append(word)
-    for length in wordlists:
-        random.shuffle(wordlists[length])
-    return wordlists
+class GetWordList:
 
-if __name__ == "__main__":
-    main()
+    def __init__(self, filename="words_alpha.txt"):
+        """
+        Initializes the WordListManager with the path to the word list file.
+        """
+        self.filename = filename
+        self.word_lists = self.create_word_lists()
+
+    def create_word_lists(self):
+        word_lists = {i: [] for i in range(3, 16)}
+        with open("words_alpha.txt") as file:
+            for word in file:
+                word = word.strip()
+                if 3 <= len(word) <= 15:
+                    word_lists[len(word)].append(word)
+        for length in word_lists:
+            random.shuffle(word_lists[length])
+        return word_lists
